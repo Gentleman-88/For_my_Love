10558fc42bf2776506b865521350bf0a3a680806
Mark <tka4mark@gmail.com>
2024-02-21 15:06:49 +0200

    Add Framer Motion

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,30 +1,48 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import css from './Home.module.css';
+import './Home.css';
 
 const HomePage = () => {
+  // function onEntry(entry) {
+  //   entry.forEach(change => {
+  //     if (change.isIntersecting) {
+  //       change.target.classList.add('element-show');
+  //     }
+  //   });
+  // }
+
+  // let options = {
+  //   threshold: [0.5],
+  // };
+  // let observer = new IntersectionObserver(onEntry, options);
+  // let elements = document.querySelectorAll('.element-animation');
+
+  // for (let elm of elements) {
+  //   observer.observe(elm);
+  // }
+
   return (
-    <div className={css.homePage}>
-      <div className={css.hello_Page_Container}>
-        <ul className={css.title}>
-          <li className={css.list_item}>Х</li>
-          <li className={css.list_item}>Е</li>
-          <li className={css.list_item}>Л</li>
-          <li className={css.list_item}>Л</li>
-          <li className={css.list_item}>О</li>
-          <li className={css.list_item}>О</li>
-          <li className={css.list_item}>У</li>
+    <div className="homePage">
+      <div className="hello_Page_Container">
+        <ul className="title">
+          <li className="list_item">Х</li>
+          <li className="list_item">Е</li>
+          <li className="list_item">Л</li>
+          <li className="list_item">Л</li>
+          <li className="list_item">О</li>
+          <li className="list_item">О</li>
+          <li className="list_item">У</li>
         </ul>
-        <p className={css.info}>Цей сайт зроблений для тебе Катюша</p>
-        <p className={css.info}>Вибирай куда хочеш першим ділом</p>
-        <ul className={css.nav_list}>
-          <NavLink to="/Love" className={css.nav_item}>
+        <p className="info">Цей сайт зроблений для тебе Катюша</p>
+        <p className="info">Вибирай куда хочеш першим ділом</p>
+        <ul className="nav_list">
+          <NavLink to="/Love" className="nav_item">
             Хронологія відносин
           </NavLink>
-          <NavLink to="/Game" className={css.nav_item}>
+          <NavLink to="/Game" className="nav_item">
             Гра
           </NavLink>
-          <NavLink to="/Photos" className={css.nav_item}>
+          <NavLink to="/Photos" className="nav_item">
             Наші фоточки
           </NavLink>
         </ul>
